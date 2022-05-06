dedabf12cda9717d4f5d5a1cfbe5af0a9a3c55d0
marcelo-ls-reis <100324942+marcelo-ls-reis@users.noreply.github.com>
2022-05-05 21:30:02 -0300

    aula 05/05/2022

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css'
 
 import Intervalo from "./components/Intervalo";
-import Card from "./components/Card";
+import Media from "./components/Media";
+import Soma from "./components/Soma";
+import Sorteio from "./components/Sorteio";
 
 function App() {
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(50);
   return (
     <div className="App">
       <h1>Projeto-Redux</h1>
 
       <div className="linha">
-        <Intervalo></Intervalo>
+        <Intervalo min={min} max={max} onMinChanged={setMin} onMaxChanged={setMax}> </Intervalo>
       </div>
 
       <div className="linha">
-      <Card title="Card 2" green>Teste 2</Card>
-      <Card title="Card 3" blue>Teste 3</Card>
-      <Card title="Card 4" purple>Teste 4</Card>
+      <Media min={min} max={max}></Media>
+      <Soma min={min} max={max}></Soma>
+      <Sorteio min={min} max={max}></Sorteio>
       </div>
 
     </div>
---
+++ src/components/Media.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Card from "./Card";
+
+export default props => {
+    const {min, max} = props;
+    return(
+        <Card title="Média de números" green>
+            <div>
+                <span>
+                    <span>Resultado: </span>
+                    <strong>{(min + max)/ 2}</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
